e27422940b5f065aa2ef7118ed91940cd2425848
Ebces <ptashinsky@tut.by>
2020-10-22 11:07:12 +0300

    refactor: correct style.css, script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const PHOTO = document.querySelector('.gallery');
+const photo = document.querySelector('.gallery');
 
-PHOTO.addEventListener('click', (e) => {
-  const ACTIVE_ELEMENT = document.querySelector('.gallery__photo--active');
-  const CLICKED_ELEMENT = e.target.closest('div');
-  const MAIN_TEXT = CLICKED_ELEMENT.querySelector('.gallery__text-content--main');
+photo.addEventListener('click', (e) => {
+  const activeElement = document.querySelector('.gallery__photo--active');
+  const clickedElement = e.target.closest('div');
+  const mainText = clickedElement.querySelector('.gallery__text-content--main');
 
-  if (ACTIVE_ELEMENT === CLICKED_ELEMENT) {
+  if (activeElement === clickedElement) {
     return;
   }
 
   setTimeout(() => {
-    MAIN_TEXT.style.margin = '100px 0';
+    mainText.style.margin = '100px 0';
   }, 300);
 
-  CLICKED_ELEMENT.classList.add('gallery__photo--active');
+  clickedElement.classList.add('gallery__photo--active');
 
-  if (ACTIVE_ELEMENT) {
-    const ACTIVE_TEXT = ACTIVE_ELEMENT.querySelector('.gallery__text-content--main');
+  if (activeElement) {
+    const activeText = activeElement.querySelector('.gallery__text-content--main');
 
-    ACTIVE_ELEMENT.classList.add('gallery__photo--deactive');
-    ACTIVE_TEXT.style.margin = '500px 0';
+    activeElement.classList.add('gallery__photo--deactive');
+    activeText.style.margin = '500px 0';
 
     setTimeout(() => {
-      ACTIVE_ELEMENT.classList.remove('gallery__photo--deactive');
-      ACTIVE_ELEMENT.classList.remove('gallery__photo--active');
+      activeElement.classList.remove('gallery__photo--deactive');
+      activeElement.classList.remove('gallery__photo--active');
     }, 500);
   }
 });
